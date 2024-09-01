0a96c70fc64a189f747198af8fd6a2d826a27e76
Giovandro Santos <vsm.giovandro@gmail.com>
2024-09-01 20:07:45 -0300

    first commit

---
+++ src/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+    }
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
